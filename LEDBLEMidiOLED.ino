62dc40add58151da47ed72ddeca28559d2c23350
bbkiwi <staf186@ext.canterbury.ac.nz>
2025-01-19 17:45:06 +1300

    Fixed to work for NODEMCU 32S 30 pin. Streamlined code, test for pedal pushs CC

--- LEDBLEMidiOLED.ino
+++ LEDBLEMidiOLED.ino
@@ -4,6 +4,7 @@
 */
 
 #include <Arduino.h>
+//#include <hardware/BLEMIDI_Transport.h>
 #include <BLEMIDI_Transport.h>
 
 #include <hardware/BLEMIDI_Client_ESP32.h>
@@ -13,7 +14,7 @@
 //#include <hardware/BLEMIDI_nRF52.h>
 //#include <hardware/BLEMIDI_ArduinoBLE.h>
 
-#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+//#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -22,13 +23,16 @@
 
 // Initialize the OLED display using Arduino Wire:
 // For TTGO Wifi + Bluetooth Battery OLED (see http://www.areresearch.net/2018/01/how-to-use-ttgo-esp32-module-with-oled.html)
-#define SDA 5
-#define SCL 4
+// and NODEMCU 32S
+//#define SDA 5
+//#define SCL 4
+// better for NODEMCU 32S 30 pin
+#define SDA 21
+#define SCL 22
 
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 
 BLEMIDI_CREATE_DEFAULT_INSTANCE(); //Connect to first server found
 
@@ -48,6 +52,16 @@ bool isConnected = false;
 char buf[400];
 uint8_t line_to_write = 56;
 uint8_t shift_of_display = 0;
+bool DampON = false;
+
+void scrollline() {
+  line_to_write += 8;
+  line_to_write %= 64;
+  shift_of_display = (8 + line_to_write) % 64;
+  display.ssd1306_command(0x40 + shift_of_display); //40h ... 7Fh
+  display.writeFillRect(0, line_to_write, 128, 8, 0); //clear line first
+  display.setCursor(0, line_to_write);
+}
 /**
    -----------------------------------------------------------------------------
    When BLE is connected, LED will turn on (indicating that connection was successful)
@@ -58,10 +72,10 @@ uint8_t shift_of_display = 0;
 void setup()
 {
   Serial.begin(115200);
-  
-//https://github.com/espressif/arduino-esp32/issues/3779
+
+  //https://github.com/espressif/arduino-esp32/issues/3779
 #ifdef ARDUINO_ARCH_ESP32
-  Wire.setPins(5, 4);
+  Wire.setPins(SDA, SCL);
 #endif
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -84,6 +98,7 @@ void setup()
   display.println(F("MIDI keys!"));
   display.display();
 
+
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
   BLEMIDI.setHandleConnected([]()
@@ -100,18 +115,65 @@ void setup()
     digitalWrite(LED_BUILTIN, LOW);
   });
 
+
+
+  //https://nickfever.com/music/midi-cc-list
+  //ON Kawai CA49
+  //MIDI CC 64  Sustain Pedal  gives continuous ≤63 off, ≥64 on
+  //MIDI CC 66  Sostenuto Pedal on 127, off 0
+  //MIDI CC 67  Soft Pedal  on 127, off 0
+
+  MIDI.setHandleControlChange([](byte channel, byte number, byte value)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.printf("--- CC %d CH %d, number:%d, value:%d", millis() - t0, channel, number, value);
+    Serial.printf("  cursor at %d, %d\n", display.getCursorX(), display.getCursorY());
+    //display.print("ON %d CH %d, number:%d, value:%d\n", millis() - t0,  channel, number, value);
+    if (number == 66 || number == 67) {
+      if (number == 66) {
+        scrollline();
+        display.print("HOLD ");
+      } else {
+        scrollline();
+        display.print("SOFT ");
+      }
+      display.print((value <= 64) ? "OFF " : "ON ");
+      display.println(millis() - t0);
+      display.display();
+    } else if (number == 64) {
+      if (value == 0) {
+        DampON = false;
+        scrollline();
+        display.print("DAMP OFF ");
+        display.println(millis() - t0);
+        display.display();
+      } else if (!DampON) {
+        DampON = true;
+        scrollline();
+        display.print("DAMP ON ");
+        display.println(millis() - t0);
+        display.display();
+      }
+    } else {
+      scrollline();
+      display.print("C-");
+      display.print(millis() - t0);
+      display.print("-");
+      display.print(number);
+      display.print("(");
+      display.print(value);
+      display.println(")");
+      display.display();
+    }
+  });
+
   MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity)
   {
     digitalWrite(LED_BUILTIN, LOW);
-    Serial.printf("ON %d CH %d, note:%d, vel:%d\n", millis() - t0, channel, note, velocity);
+    Serial.printf("ON %d CH %d, note:%d, vel:%d", millis() - t0, channel, note, velocity);
     Serial.printf("  cursor at %d, %d\n", display.getCursorX(), display.getCursorY());
     //display.print("ON %d CH %d, note:%d, vel:%d\n", millis() - t0,  channel, note, velocity);
-    line_to_write += 8;
-    line_to_write %= 64;
-    shift_of_display = (8 + line_to_write) % 64;
-    display.ssd1306_command(0x40 + shift_of_display); //40h ... 7Fh
-    display.writeFillRect(0, line_to_write, 128, 8, 0); //clear line first
-    display.setCursor(0, line_to_write);
+    scrollline();
     display.print("D-");
     display.print(millis() - t0);
     display.print("-");
@@ -124,14 +186,9 @@ void setup()
   MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity)
   {
     digitalWrite(LED_BUILTIN, HIGH);
-    Serial.printf("OFF %d CH %d, note:%d, vel:%d\n", millis() - t0,  channel, note, velocity);
+    Serial.printf("OFF %d CH %d, note:%d, vel:%d", millis() - t0,  channel, note, velocity);
     Serial.printf("  cursor at %d, %d\n", display.getCursorX(), display.getCursorY());
-    line_to_write += 8;
-    line_to_write %= 64;
-    shift_of_display = (8 + line_to_write) % 64;
-    display.ssd1306_command(0x40 + shift_of_display);
-    display.writeFillRect(0, line_to_write, 128, 8, 0);  //clear line first
-    display.setCursor(0, line_to_write);
+    scrollline();
     display.print("U-");
     display.print(millis() - t0);
     display.print("-");
@@ -160,6 +217,8 @@ void setup()
 // -----------------------------------------------------------------------------
 void loop()
 {
+
+
   //MIDI.read();  // This function is called in the other task
 
   //  if (isConnected && (millis() - t0) > 1000)
